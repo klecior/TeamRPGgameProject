76622b811fd9cf1164a70acdd2ace058434f4a33
Rhys Matthew Kristopher Johnson <white0glint@gmail.co.uk>
2016-05-23 17:12:27 +0100

    Messages changed

--- TeamProjectRPGgame/messages.h
+++ TeamProjectRPGgame/messages.h
@@ -3,7 +3,7 @@ enum messages
 {
 	noEvent,changeStateMessage, getCurrentStateMessage,getMousePosMessage, leftMouseClickMessage, changeHealthMessage, changeStaminaMessage,
 	playerControlsMessage, entityCreatedMessage, entityDeletedMessage,hitPlayerMessage, castSpellMessage, getPlayerStatsMessage,moveWorldMessage,
-	objectsCollidedMessage, quitGameMessage
+	objectsCollidedMessage, quitGameMessage, addStatusEffectMessage, removeStatusEffectMessage, removeAllStatusEffectMessage, statusEffectEndedMessage
 };
 
 enum spellNames
@@ -13,5 +13,5 @@ enum spellNames
 
 enum statusEffectMessages
 {
-	onFire, speedBoost, armourBoost, healing, testStatus, addStatusEffectMessage, removeStatusEffectMessage, removeAllStatusEffectMessage, statusEffectEndedMessage
+	onFire, speedBoost, armourBoost, healing, testStatus
 };
